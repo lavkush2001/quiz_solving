bac8c8124b4492332bba731fbf33a688da01bc6c
Lavkush Gour <59250257+lavkush2001@users.noreply.github.com>
2021-09-16 15:39:24 +0530

    Add files via upload
    quiz_website

---
+++ quiz.js
@@ -0,0 +1,105 @@
+var questionBank= [
+    {
+        question : 'what is the fourth planet in the solar system',
+        option : ['mars','jupiter','Earh','surtan'],
+        answer : 'mars'
+    },
+    {
+        question : 'which is the following is not a data type in c language',
+        option : ['string', 'float','list','char'],
+        answer : 'list'
+    },
+    {
+        question : 'Which loop is better to iterate array in js',
+        option : ['forEach','for in','for of','none'],
+        answer : 'for in'
+    },
+    {
+        question : 'which is programming language',
+        option : ['HTML','Css','Java','none'],
+        answer : 'Java'
+    },
+    {
+        question : 'who Create the Python langauage',
+        option : ['none','Tim Berners','dennish','Guido van'],
+        answer : 'Guido van'
+    }
+]
+
+var question= document.getElementById('question');
+var quizContainer= document.getElementById('quiz-container');
+var scorecard= document.getElementById('scorecard');
+var option0= document.getElementById('option0');
+var option1= document.getElementById('option1');
+var option2= document.getElementById('option2');
+var option3= document.getElementById('option3');
+var next= document.querySelector('.next');
+var points= document.getElementById('score');
+var span= document.querySelectorAll('span');
+var i=0;
+var score= 0;
+
+//function to display questions
+function displayQuestion(){
+    for(var a=0;a<span.length;a++){
+        span[a].style.background='none';
+    }
+    question.innerHTML= 'Q.'+(i+1)+' '+questionBank[i].question;
+    option0.innerHTML= questionBank[i].option[0];
+    option1.innerHTML= questionBank[i].option[1];
+    option2.innerHTML= questionBank[i].option[2];
+    option3.innerHTML= questionBank[i].option[3];
+    stat.innerHTML= "Question"+' '+(i+1)+' '+'of'+' '+questionBank.length;
+}
+
+//function to calculate scores
+function calcScore(e){
+    if(e.innerHTML===questionBank[i].answer && score<questionBank.length)
+    {
+        score= score+1;
+        document.getElementById(e.id).style.background= 'limegreen';
+    }
+    else{
+        document.getElementById(e.id).style.background= 'tomato';
+    }
+    setTimeout(nextQuestion,300);
+}
+
+//function to display next question
+function nextQuestion(){
+    if(i<questionBank.length-1)
+    {
+        i=i+1;
+        displayQuestion();
+    }
+    else{
+        points.innerHTML= score+ '/'+ questionBank.length;
+        quizContainer.style.display= 'none';
+        scoreboard.style.display= 'block'
+    }
+}
+
+//click events to next button
+next.addEventListener('click',nextQuestion);
+
+//Back to Quiz button event
+function backToQuiz(){
+    location.reload();
+}
+
+//function to check Answers
+function checkAnswer(){
+    var answerBank= document.getElementById('answerBank');
+    var answers= document.getElementById('answers');
+    answerBank.style.display= 'block';
+    scoreboard.style.display= 'none';
+    for(var a=0;a<questionBank.length;a++)
+    {
+        var list= document.createElement('li');
+        list.innerHTML= questionBank[a].answer;
+        answers.appendChild(list);
+    }
+}
+
+
+displayQuestion();
